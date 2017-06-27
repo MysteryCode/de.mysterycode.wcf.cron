49dae13cb2ec9c96b11105cfd72fcaa1d440d4ba
Morik <Sebastian.Zimmer@gmx.net>
2017-06-27 20:19:18 +0200

    Disable cron execution during offline mode

--- files/cron.php
+++ files/cron.php
@@ -6,20 +6,22 @@ use wcf\data\cronjob\CronjobAction;
 use wcf\system\background\BackgroundQueueHandler;
 use wcf\system\WCF;
 
-$applicationList = new ApplicationList();
-$applicationList->readObjects();
+if (!defined('OFFLINE') || !OFFLINE) {
+	$applicationList = new ApplicationList();
+	$applicationList->readObjects();
 
-foreach ($applicationList->getObjects() as $application) {
-	WCF::loadRuntimeApplication($application->packageID);
-}
+	foreach ($applicationList->getObjects() as $application) {
+		WCF::loadRuntimeApplication($application->packageID);
+	}
 
-$action = new CronjobAction([], 'executeCronjobs');
-$action->executeAction();
+	$action = new CronjobAction([], 'executeCronjobs');
+	$action->executeAction();
 
-// send up to 5 outstanding mails
-$limit = min(5, BackgroundQueueHandler::getInstance()->getRunnableCount());
-for ($i=0; $i < $limit; $i++) {
-	BackgroundQueueHandler::getInstance()->performNextJob();
+	// send up to 5 outstanding mails
+	$limit = min(5, BackgroundQueueHandler::getInstance()->getRunnableCount());
+	for ($i=0; $i < $limit; $i++) {
+		BackgroundQueueHandler::getInstance()->performNextJob();
+	}
 }
 
 WCF::getSession()->delete();
