5b19502d5d655d4464615881b3fe315236fec844
Hanashi <hanashi@hanashi.eu>
2022-05-23 10:03:31 +0200

    change to psr-12 code style

--- files/cron.php
+++ files/cron.php
@@ -1,4 +1,7 @@
 <?php
+
+// phpcs:disable PSR1.Files.SideEffects
+
 define('PACKAGE_ID', 1);
 require_once(__DIR__ . '/global.php');
 use wcf\data\application\ApplicationList;
@@ -7,21 +10,21 @@ use wcf\system\background\BackgroundQueueHandler;
 use wcf\system\WCF;
 
 if (!defined('OFFLINE') || !OFFLINE) {
-	$applicationList = new ApplicationList();
-	$applicationList->readObjects();
+    $applicationList = new ApplicationList();
+    $applicationList->readObjects();
 
-	foreach ($applicationList->getObjects() as $application) {
-		WCF::loadRuntimeApplication($application->packageID);
-	}
+    foreach ($applicationList->getObjects() as $application) {
+        WCF::loadRuntimeApplication($application->packageID);
+    }
 
-	$action = new CronjobAction([], 'executeCronjobs');
-	$action->executeAction();
+    $action = new CronjobAction([], 'executeCronjobs');
+    $action->executeAction();
 
-	// send up to 5 outstanding mails
-	$limit = min(5, BackgroundQueueHandler::getInstance()->getRunnableCount());
-	for ($i=0; $i < $limit; $i++) {
-		BackgroundQueueHandler::getInstance()->performNextJob();
-	}
+    // send up to 5 outstanding mails
+    $limit = min(5, BackgroundQueueHandler::getInstance()->getRunnableCount());
+    for ($i = 0; $i < $limit; $i++) {
+        BackgroundQueueHandler::getInstance()->performNextJob();
+    }
 }
 
 WCF::getSession()->delete();
